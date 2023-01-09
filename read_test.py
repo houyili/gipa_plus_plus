67e746c959a611a1e5a3a593400d249bd06e0f18
lihouyi <lihouyi@kuaishou.com>
2023-01-09 16:39:51 +0800

    add practice_3.py

--- read_test.py
+++ read_test.py
@@ -8,5 +8,5 @@ n_node_feats, n_edge_feats, n_classes = 0, 8, 112
 graph, labels, train_idx, val_idx, test_idx, evaluator = load_data(dataset, root_path)
 graph, labels = preprocess(graph, labels, use_sparse=True)
 
-print(graph.edata["feat"][0:10])
-print(graph.edata["sparse"][0:10])
+print(graph.edata["feat"][0])
+print(graph.edata["sparse"][0])
