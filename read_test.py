60573132df2174bd0c2ceead40df30183f62797a
lihouyi <lihouyi@kuaishou.com>
2023-01-10 20:35:02 +0800

    edge_reverse

--- read_test.py
+++ read_test.py
@@ -1,3 +1,4 @@
+import torch
 from data_loader import load_data, preprocess
 
 # root_path = "/Users/lihouyi/Documents/opensource/data_source/proteins"
@@ -6,8 +7,20 @@ dataset = "ogbn-proteins"
 n_node_feats, n_edge_feats, n_classes = 0, 8, 112
 
 graph, labels, train_idx, val_idx, test_idx, evaluator = load_data(dataset, root_path)
-graph, labels = preprocess(graph, labels, sparse_encoder="hard1")
+graph, labels = preprocess(graph, labels, sparse_encoder="hard_30")
 
-print(graph.srcdata["sparse"][0])
+print(train_idx)
+print(val_idx)
+print(test_idx)
+
+print(torch.mean(graph.ndata["sparse"][train_idx], dim=-1))
+print(torch.mean(graph.ndata["sparse"][val_idx], dim=-1))
+print(torch.mean(graph.ndata["sparse"][test_idx], dim=-1))
+
+
+print(torch.mean(graph.ndata["feat"][train_idx], dim=-1))
+print(torch.mean(graph.ndata["feat"][val_idx], dim=-1))
+print(torch.mean(graph.ndata["feat"][test_idx], dim=-1))
+
 # print(graph.edata["feat"][0])
 # print(graph.edata["sparse"][0])
