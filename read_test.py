5282439e3890921f36d13f332e99643c9e550194
lihouyi <lihouyi@kuaishou.com>
2023-01-10 20:52:52 +0800

    read_test.py

--- read_test.py
+++ read_test.py
@@ -11,18 +11,16 @@ graph, labels = preprocess(graph, labels, sparse_encoder="hard_30")
 
 for id in [train_idx, val_idx, test_idx]:
     print(id)
-    sparse = torch.mean(graph.ndata["sparse"][id], dim=0)
-    dense = torch.mean(graph.ndata["feat"][id], dim=0)
-    print(sparse.shape)
-    print(dense.shape)
-    # str = ""
-    # for i in range(len(sparse.tolist())):
-    #     str = str + ", %.03f" %sparse[i]
-    # # print(str)
-    # str = ""
-    # for i in range(len(dense.tolist())):
-    #     str = str + ", %.03f" % dense[i]
+    sparse = torch.mean(graph.ndata["sparse"][id], dim=0).tolist()
+    dense = torch.mean(graph.ndata["feat"][id], dim=0).tolist()
+    str = ""
+    for i in range(len(sparse)):
+        str = str + ", %.03f" %sparse[i]
     # print(str)
+    str = ""
+    for i in range(len(dense)):
+        str = str + ", %.03f" % dense[i]
+    print(str)
 
 # print(graph.edata["feat"][0])
 # print(graph.edata["sparse"][0])
