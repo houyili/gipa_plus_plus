1b86b9ec36aa9ae693d5611814360be367623cb7
lihouyi <lihouyi@kuaishou.com>
2023-01-10 20:46:20 +0800

    read_test.py

--- read_test.py
+++ read_test.py
@@ -9,18 +9,18 @@ n_node_feats, n_edge_feats, n_classes = 0, 8, 112
 graph, labels, train_idx, val_idx, test_idx, evaluator = load_data(dataset, root_path)
 graph, labels = preprocess(graph, labels, sparse_encoder="hard_30")
 
-print(train_idx)
-print(val_idx)
-print(test_idx)
-
-print(torch.mean(graph.ndata["sparse"][train_idx], dim=-1))
-print(torch.mean(graph.ndata["sparse"][val_idx], dim=-1))
-print(torch.mean(graph.ndata["sparse"][test_idx], dim=-1))
-
-
-print(torch.mean(graph.ndata["feat"][train_idx], dim=-1))
-print(torch.mean(graph.ndata["feat"][val_idx], dim=-1))
-print(torch.mean(graph.ndata["feat"][test_idx], dim=-1))
+for id in [train_idx, val_idx, test_idx]:
+    print(id)
+    sparse = torch.mean(graph.ndata["sparse"][id], dim=-1).tolist()
+    dense = torch.mean(graph.ndata["feat"][id], dim=-1).tolist()
+    str = ""
+    for i in range(len(sparse)):
+        str = str + ", %.03f" %sparse[i]
+    print(str)
+    str = ""
+    for i in range(len(dense)):
+        str = str + ", %.03f" % dense[i]
+    print(str)
 
 # print(graph.edata["feat"][0])
 # print(graph.edata["sparse"][0])
