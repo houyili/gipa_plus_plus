3c5a9af500758b5db5a94078f86902db1adaa96c
lihouyi <lihouyi@kuaishou.com>
2023-01-11 01:41:16 +0800

    first_layer_norm

---
+++ script/run_layer6_sp_hard30_norm2_idp_150.sh
@@ -0,0 +1,27 @@
+cd "$(dirname $0)"
+python -u ../train_gipa.py \
+    --train-partition-num 6 \
+    --eval-partition-num 2 \
+    --eval-times 1 \
+    --lr 0.01 \
+    --advanced-optimizer \
+    --n-epochs 1500 \
+    --n-heads 20 \
+    --n-layers 6 \
+    --dropout 0.4 \
+    --n-hidden 50 \
+    --input-drop 0.1 \
+    --edge-drop 0.1 \
+    --edge-agg-mode "single_softmax" \
+    --edge-att-act "none" \
+    --norm none \
+    --edge-emb-size 16\
+    --gpu 5 \
+    --first-hidden 150 \
+    --use-sparse-fea \
+    --sparse-encoder "hard_30" \
+    --input-norm \
+    --first-layer-norm \
+    --first-layer-act "leaky_relu" \
+    --feature-drop 0.4 \
+    --log-file-name="train_gipa_layer6_sp_hard30_norm2_idp_150"
